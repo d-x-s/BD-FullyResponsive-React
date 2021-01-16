bc0534881d1f1406ede97236c63bd4bae1ae5231
dsong04 <davissong01@gmail.com>
2021-01-16 00:45:28 -0800

    Pages Complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+
 import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -12,6 +16,9 @@ function App() {
       <Switch>
         {/* "/"" is home component */}
         <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/sign-up" component={SignUp} />
       </Switch>
     </Router>
   );

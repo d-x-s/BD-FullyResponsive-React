edd9b123a9b989334e212560af92b0438eecb22a
dsong04 <davissong01@gmail.com>
2021-01-15 18:48:19 -0800

    Live Background Complete

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../App.css";
+import HeroDynamic from "../HeroDynamic";
+
+function Home() {
+  return (
+    <>
+      <HeroDynamic />
+    </>
+  );
+}
+
+export default Home;
